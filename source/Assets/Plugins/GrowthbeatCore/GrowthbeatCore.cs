47276842578ae8f96f3d16b16c751cd0c02ee1cc
Shigeru Ogawa <ogawa@sirok.co.jp>
2015-03-02 18:37:40 +0900

    change singleton instance GrowthbeatCore.cs

--- source/Assets/Plugins/GrowthbeatCore/GrowthbeatCore.cs
+++ source/Assets/Plugins/GrowthbeatCore/GrowthbeatCore.cs
@@ -4,15 +4,22 @@ using System.Runtime.InteropServices;
 using System;
 using System.Collections.Generic;
 
-public class GrowthbeatCore {
-	
-	public static void Initialize(string applicationId, string credentialId) {
-				#if UNITY_ANDROID
-					GrowthbeatCoreAndroid.Initialize(applicationId, credentialId);
-				#elif UNITY_IPHONE
-					GrowthbeatCoreIOS.Initialize(applicationId, credentialId); 
-				#endif
+public class GrowthbeatCore
+{
+	private static GrowthbeatCore instance = new GrowthbeatCore ();
 
-		}
-		
+	public static void GetInstance ()
+	{
+		return GrowthbeatCore.instance;
+	}
+
+	public void Initialize (string applicationId, string credentialId)
+	{
+		#if UNITY_ANDROID
+			GrowthbeatCoreAndroid.Initialize(applicationId, credentialId);
+		#elif UNITY_IPHONE
+			GrowthbeatCoreIOS.Initialize(applicationId, credentialId);
+		#endif
+
+	}
 }
